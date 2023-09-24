8489257381fb57b911fe08cc02c513df1c771c4b
Hun-ho Kim <hunho.kim425@gmail.com>
2023-09-25 08:42:13 +0900

    Refactor init.lua

--- init.lua
+++ init.lua
@@ -1,80 +1,31 @@
-vim.cmd [[
-colorscheme nightfox
+-- Recommended by nvim-tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
 
-" User command to open the configuration file
-:command Neovimconfig edit ~/.config/nvim/init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-" Tab size
-set tabstop=2
-set expandtab
-set shiftwidth=2
+require("lazy").setup({
+	{
+		import = "plugins",
+		change_detection = {
+			enabled = false
+		}
+	}
+})
 
-autocmd Filetype go setlocal tabstop=4 shiftwidth=4
-
-" Line number
-set number
-set cursorline
-set relativenumber
-
-" map <Esc> to other keys
-imap jk <Esc>
-
-" Nvim-tree
-nnoremap <C-n> :NvimTreeToggle<CR>
-nnoremap <leader>r :NvimTreeRefresh<CR>
-nnoremap <leader>n :NvimTreeFindFile<CR>
-set termguicolors " this variable must be enabled for colors to be applied properly
-
-" Telescope
-nnoremap <leader>ff <cmd>Telescope find_files<cr>
-nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-nnoremap <leader>fb <cmd>Telescope buffers<cr>
-nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-
-" nerdcommenter
-filetype plugin on
-
-" <Esc> to remove highlight
-nnoremap <Esc> :noh<CR>
-
-" Clipboard sharing on mac
-set clipboard=unnamedplus
-
-" Vim-plug
-call plug#begin()
-  Plug 'neoclide/coc.nvim', {'branch': 'release'}
-
-  Plug 'hrsh7th/cmp-buffer'
-  Plug 'hrsh7th/cmp-path'
-  Plug 'hrsh7th/cmp-cmdline'
-  Plug 'hrsh7th/nvim-cmp'
-
-  Plug 'jiangmiao/auto-pairs'
-call plug#end()
-
-source ~/.config/nvim/vim/coc.vim
-source ~/.config/nvim/vim/coc-snippets.vim
-]]
-
-require 'plugins'
-require 'nvim-cmp'
-
-require 'nvim-tree'.setup {
-  git = {
-    enable = false,
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-}
-require 'lualine'.setup {}
-
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "python", "vim", "go", "javascript", "java" },
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
+require("config.init")
+require("config.keymap")
---
+++ lua/config/init.lua
@@ -0,0 +1,15 @@
+vim.cmd.colorscheme "nightfox"
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_create_user_command('Neovimconfig', 'edit ~/.config/nvim/init.lua', {})
+
+vim.keymap.set('n', '<Esc>', ':noh<CR>')
+vim.keymap.set('i', 'jk', '<Esc>')
