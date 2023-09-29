787a368c71076abfd5cfc412a301a5c0ba70f3c7
Hun-ho Kim <hunho.kim425@gmail.com>
2023-09-29 19:32:30 +0900

    Add diagnostic config

--- lua/config/cmp-lspconfig.lua
+++ lua/config/cmp-lspconfig.lua
@@ -10,8 +10,8 @@ local select_opts = { behavior = cmp.SelectBehavior.Select }
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
 )
 
 cmp.setup({
