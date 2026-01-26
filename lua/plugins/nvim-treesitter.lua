32a41671dba402c434a40a26530e9715fde4f1a1
Hun-ho Kim <hunho.kim425@gmail.com>
2026-01-26 15:40:31 +0900

    Update ensure_installed for nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "lua", "go", "rust", "html", "javascript", "ruby" },
+			ensure_installed = { "lua", "go", "rust", "ruby", "c", "cpp" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
