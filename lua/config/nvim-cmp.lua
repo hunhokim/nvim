bb6c53666df911f843d676d3c685674373765b3b
Hun-ho Kim <hunho.kim425@gmail.com>
2026-01-28 22:12:43 +0900

    Add nvim-cmp

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,9 +1,26 @@
 local cmp = require 'cmp'
 
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+		end,
+	},
 	window = {
 		completion = cmp.config.window.bordered(),
-	}
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+	}, {
+		{ name = 'buffer' },
+	})
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -21,5 +38,6 @@ cmp.setup.cmdline(':', {
 		{ name = 'path' }
 	}, {
 		{ name = 'cmdline' }
-	})
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false }
 })
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,4 +3,5 @@ return {
 	'hrsh7th/cmp-path',
 	'hrsh7th/cmp-cmdline',
 	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-nvim-lsp',
 }
